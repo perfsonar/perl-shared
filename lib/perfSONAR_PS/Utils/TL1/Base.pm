efe676c10b6add55a8a1e6c7a6259d9f07106c39
aaron <aaron@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2009-03-17 18:25:45 +0000

    Add some documentation to the TL1 Base Class.
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/RELEASE_3.1@2599 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- lib/perfSONAR_PS/Utils/TL1/Base.pm
+++ lib/perfSONAR_PS/Utils/TL1/Base.pm
@@ -3,6 +3,20 @@ package perfSONAR_PS::Utils::TL1::Base;
 use warnings;
 use strict;
 
+our $VERSION = 3.1;
+
+=head1 NAME
+
+perfSONAR_PS::Utils::TL1::Base
+
+=head1 DESCRIPTION
+
+A module that provides the Base from which all the TL1 modules derive. This
+object is not meant to be used directly, but should be inherited by classes
+that implement the various TL1 dialects.
+
+=cut
+
 use POSIX;
 use Net::Telnet;
 use Data::Dumper;
@@ -13,6 +27,9 @@ use perfSONAR_PS::Utils::ParameterValidation;
 
 use fields 'USERNAME', 'PASSWORD', 'TYPE', 'ADDRESS', 'PORT', 'CACHE_DURATION', 'CACHE_TIME', 'LOGGER', 'MACHINE_TIME', 'LOCAL_MACHINE_TIME', 'PROMPT', 'CTAG', 'TELNET', 'MESSAGES', 'STATUS';
 
+=head2 new()
+Allocates a new object.
+=cut
 sub new {
     my ($class) = @_;
 
@@ -23,6 +40,9 @@ sub new {
     return $self;
 }
 
+=head2 initialize({ type => 1, address => 1, port => 1, username => 1, password => 1, cache_time => 1, prompt => 1, ctag => 0)
+Initializes the object according to the specified parameters. If the ctag is left unstated, it is randomly generated.
+=cut
 sub initialize {
     my ($self, @params) = @_;
 
@@ -61,98 +81,69 @@ sub initialize {
     return $self;
 }
 
+=head2 getType()
+Returns the TL1 dialect of this object.
+=cut
 sub getType {
     my ($self) = @_;
 
     return $self->{TYPE};
 }
 
-sub setType {
-    my ($self, $type) = @_;
-
-    $self->{TYPE} = $type;
-
-    return;
-}
-
-sub setUsername {
-    my ($self, $username) = @_;
-
-    $self->{USERNAME} = $username;
-
-    return;
-}
-
+=head2 getUsername()
+Returns the username used to login to the TL1 device
+=cut
 sub getUsername {
     my ($self) = @_;
 
     return $self->{USERNAME};
 }
 
-sub setPassword {
-    my ($self, $password) = @_;
-
-    $self->{PASSWORD} = $password;
-
-    return;
-}
-
+=head2 getPassword()
+Returns the password used to login to the TL1 device
+=cut
 sub getPassword {
     my ($self) = @_;
 
     return $self->{PASSWORD};
 }
 
-sub setAddress {
-    my ($self, $address) = @_;
-
-    $self->{ADDRESS} = $address;
-
-    return;
-}
-
+=head2 getAddress()
+Returns the address of the TL1 device
+=cut
 sub getAddress {
     my ($self) = @_;
 
     return $self->{ADDRESS};
 }
 
-sub setAgent {
-    my ($self, $agent) = @_;
-
-    $self->{TL1AGENT} = $agent;
-
-    return;
-}
-
-sub getAgent {
-    my ($self) = @_;
-
-    return $self->{TL1AGENT};
-}
-
-sub setCacheTime {
-    my ($self, $time) = @_;
-
-    $self->{CACHE_TIME} = $time;
-
-    return $self->{CACHE_TIME};
-}
-
+=head2 getCacheTime()
+Returns the length of time that information is cached by the object
+=cut
 sub getCacheTime {
     my ($self) = @_;
 
     return $self->{CACHE_TIME};
 }
 
+=head2 login()
+Internal function used to login to the service. MUST be overridden by implementing classes.
+=cut
 sub login {
     die("This method must be overriden by a subclass");
 }
 
+=head2 logout()
+Internal function used to logout of the service. CAN be overridden by implementing class if more is required than just disconnecting from the machine.
+=cut
 sub logout {
     return;
 }
 
+=head2 connect({ inhibitMessages => 0 })
+Connects and logs into the device. Returns 0 on success and -1 on failure. AS
+messages can be disabled by setting the "inhibitMessages" key to 1.
+=cut
 sub connect {
     my ($self, @params) = @_;
     my $parameters = validate(@params,
@@ -182,6 +173,9 @@ sub connect {
     return 0;
 }
 
+=head2 disconnect({ })
+Logs off and disconnects from the device. 
+=cut
 sub disconnect {
     my ($self) = @_;
 
@@ -203,6 +197,9 @@ sub disconnect {
     return;
 }
 
+=head2 refresh_connection({ })
+Keeps the connection alive by sending a PING-PONG like message to the device.
+=cut
 sub refresh_connection {
     my ($self) = @_;
 
@@ -214,8 +211,14 @@ sub refresh_connection {
     return (0, "");
 }
 
+=head2 send_cmd($cmd)
+Sends the specified command to the device and returns the response. It returns
+(0, \@lines) where @lines is an array of the lines from the response. On error,
+it will return (-1, [error msg]) where [error msg] is a string containing an
+error message.
+=cut
 sub send_cmd {
-    my ($self, $cmd, $is_connecting) = @_;
+    my ($self, $cmd) = @_;
     
     if (not $self->{TELNET}) {
         return (-1, undef);
@@ -270,6 +273,12 @@ sub send_cmd {
     return ($successStatus, \@retLines);
 }
 
+=head2 waitMessage({ type => 1, timeout => 0 })
+Waits for a message of the specified type from the device. The types are
+specific to the class implementing the TL1 dialect. However, common ones are
+"response", "alarm" and "event". The timeout parameter allows specifying how
+many seconds to wait before returning an error message.
+=cut
 sub waitMessage {
     my ($self, @args) = @_;
     my $args = validateParams(@args, 
@@ -323,6 +332,11 @@ sub waitMessage {
     return (0, $lines);
 }
 
+=head2 readMessage({ timeout => 0 })
+Waits for any message from the device. Returns (0, \@lines) on success, (-1,
+[error msg]) on error, (0, undef) if the connection closed and (1, undef) if a
+timeout occurred.
+=cut
 sub readMessage {
     my ($self, @args) = @_;
     my $args = validateParams(@args, 
@@ -375,6 +389,11 @@ sub readMessage {
     }
 }
 
+=head2 processMessage(\@lines)
+An internal function which categorizes a message and then places it into a
+buffer mailbox. As part of this, it also updates the internal machine time if
+it sees a machine time.
+=cut
 sub processMessage {
     my ($self, $lines) = @_;
 
@@ -404,6 +423,9 @@ sub processMessage {
     return 0;
 }
 
+=head2 clearMessages()
+Gets rid of all the queued up messages.
+=cut
 sub clearMessages {
     my ($self) = @_;
 
@@ -412,6 +434,11 @@ sub clearMessages {
     return;
 }
 
+=head2 categorizeMessage()
+Gets run against the message and decides what type that message is. This
+version is a default, and handles categorizing messages as alarms, events,
+responses or unknown.
+=cut
 sub categorizeMessage {
     my ($self, $lines) = @_;
 
@@ -438,6 +465,10 @@ sub categorizeMessage {
     return "other";
 }
 
+=head2 setMachineTime($time)
+Internal function used to set the current machine time according to the times
+seen on the machine.
+=cut
 sub setMachineTime {
     my ($self, $time) = @_;
 
@@ -467,6 +498,10 @@ sub setMachineTime {
     return;
 }
 
+=head2 getMachineTime()
+Returns the current machine time, based on the last time the object saw from
+the device, and what time it saw. Returns it as a human-readable string.
+=cut
 sub getMachineTime {
     my ($self) = @_;
 
@@ -485,6 +520,10 @@ sub getMachineTime {
     return $readable_time;
 }
 
+=head2 getMachineTime_TS()
+Returns the current machine time, based on the last time the object saw from
+the device, and what time it saw. Returns it as a timestamp.
+=cut
 sub getMachineTime_TS {
     my ($self) = @_;
 
@@ -494,6 +533,11 @@ sub getMachineTime_TS {
     return $machine_ts;
 }
 
+=head2 convertPMDateTime()
+Converts a performance-metric date/time into an absolute data/time. The PM
+date/time don't inclue a year, so this attemps to guess it based on the
+device's time.
+=cut
 sub convertPMDateTime {
 	my ($self, $date, $time) = @_;
 
@@ -521,6 +565,9 @@ sub convertPMDateTime {
 	return sprintf "%4d-%02d-%02d %02d:%02d:%02d", $year,$month,$day,$hour,$minute,0;
 }
 
+=head2 convertTimeStringToTimestamp()
+Internal function that converts a time string into a unix timestamp.
+=cut
 sub convertTimeStringToTimestamp {
 	my ($self, $time_str) = @_;
 
@@ -533,6 +580,10 @@ sub convertTimeStringToTimestamp {
     return POSIX::mktime($second, $minute, $hour, $day, $month - 1, $year - 1900, 0, 0);
 }
 
+=head2 convertMachineTSToLocalTS()
+Function that maps a machine timestamp to a local timestamp. Used if the
+machine is not synchronized with NTP.
+=cut
 sub convertMachineTSToLocalTS {
     my ($self, $machine_timestamp) = @_;
 
@@ -542,5 +593,48 @@ sub convertMachineTSToLocalTS {
 }
 
 1;
+
+__END__
+
+=head1 SEE ALSO
+
+L<POSIX>,L<Net::Telnet>,L<Log::Log4perl>,L<perfSONAR_PS::Utils::ParameterValidation>
+
+
+To join the 'perfSONAR Users' mailing list, please visit:
+
+  https://mail.internet2.edu/wws/info/perfsonar-user
+
+The perfSONAR-PS subversion repository is located at:
+
+  http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk
+
+Questions and comments can be directed to the author, or the mailing list.
+Bugs, feature requests, and improvements can be directed here:
+
+  http://code.google.com/p/perfsonar-ps/issues/list
+
+=head1 VERSION
+
+$Id$
+
+=head1 AUTHOR
+
+Aaron Brown, aaron@internet2.edu
+
+=head1 LICENSE
+
+You should have received a copy of the Internet2 Intellectual Property Framework
+along with this software.  If not, see
+<http://www.internet2.edu/membership/ip.html>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004-2009, Internet2 and the University of Delaware
+
+All rights reserved.
+
+=cut
+
 
 # vim: expandtab shiftwidth=4 tabstop=4
