8a9223542df9f39e091179230a45799de5936d84
maxim <maxim@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2009-12-16 22:42:47 +0000

    added ability to attach pinger logfile in case of failure
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@3802 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- client/pinger_service_check.pl
+++ client/pinger_service_check.pl
@@ -6,34 +6,79 @@ use warnings;
 use perfSONAR_PS::Client::PingER;
 use Data::Dumper;
 use Getopt::Long;
-use English;
+use English '-no_match_vars';
 use POSIX qw(strftime);
 use JSON::XS;
+use Mail::Sender;
+use Sys::Hostname;
+use Pod::Usage; 
 
-###use  Log::Log4perl  qw(:easy); 
-my $url = 'http://localhost:8075/perfSONAR_PS/services/pinger/ma';
-my ($debug, $help, $data);
-my $ok = GetOptions (
-                'url=s'       => \$url,
-		'data'        => \$data,
-                'help|?|h'    => \$help
-        );
-if(!$ok || !$url  || $help) {
-    print " $0: sends an  XML request over SOAP to the pinger MA and prints response or returns conclusion about service's health \n";
-    print " $0   [--url=<pinger_MA_url, default is localhost> --debug|-d  --verbose|v --data] \n";
-    print " $0    --data - to check data for every metadata id for the past 30 minutes \n";
-    print " $0    --check - to return conclusion about health \n";
-    exit 0;
-}
-#my $level = $INFO;
-#
-#if ($debug) {
-#    $level = $DEBUG;    
-#}
+our $VERSION = 3.1.1;
 
-#Log::Log4perl->easy_init($level);
-#my $logger = get_logger("pinger_client");
+=head1 NAME
+
+pinger_service_check.pl -  PingER MA service checker
+
+=head1 DESCRIPTION
+
+Connects to the PingER MA and check healths and either returns JSON encoded string with status or send email with notification if configured
+
+=head1 SYNOPSIS
+
+  #   check health of the PingER service at xenmon.fnal.gov:8075 and send notification to maxim@fnal.gov over smtp.fnal.gov if check failed
+  #
+ ./pinger_service_check.pl --url='http://xenmon.fnal.gov:8075/perfSONAR_PS/services/pinger/ma'--mail='maxim@fnal.gov' --smtp='smtp.fnal.gov'
   
+  #
+  #    check health of the PingER service at xenmon.fnal.gov:8075 and print/return JSON encoded string
+  #
+  ./pinger_service_check.pl --url='http://xenmon.fnal.gov:8075/perfSONAR_PS/services/pinger/ma'
+ 
+=head1 OPTIONS
+
+=head2 C<--help|-h>  Usage Info
+
+=head2 C<--url|-u>  URL
+
+URL of the PIngER MA to test, defaults to B<http://localhost:8075/perfSONAR_PS/services/pinger/ma>
+
+=head2 C<--mail|-m> EMAIL address
+
+EMAIL address to send notification to, defaults to B<undef>
+
+=head2 C<--smtp|-s> SMTP server address
+
+SMTP address of the server to relay email via, defaults to default value on the host
+This option B<ONLY> processed when I<--mail|-m> is set.
+
+=head2 C<--log|l> LOG file full filename
+
+specify full filename for the pinger log file to send with email 
+
+=cut
+
+my $url = 'http://localhost:8075/perfSONAR_PS/services/pinger/ma';
+our ($MAIL, $email, $help, $smtp, $logfile);
+my $ok = GetOptions (
+                'url|u=s'  => \$url,
+		'mail|m=s' => \$email,
+		'smtp|s=s' =>  \$smtp,
+		'log|l=s'  => \$logfile,     
+                'help|?|h' => \$help
+        ) or pod2usage(-verbose => 1);
+if($help || ($logfile && !(-e $logfile)) || ($email  && $email !~ /^[\w\.]+\@[\w\-\.]+$/)) {
+    pod2usage(-verbose => 2);
+}
+if($email) {
+   
+    $smtp ||= 'localhost';
+    $MAIL = Mail::Sender->new({smtp => $smtp, 
+                               from => 'pinger_ma@' . hostname()
+	  		     });
+    die $Mail::Sender::Error unless ref $MAIL;
+    $logfile ||= '/var/log/perfsonar/pinger.log';
+}
+
 my ($result, $metaids);
 my $ma = new perfSONAR_PS::Client::PingER( { instance => $url } );
 eval {
@@ -97,14 +142,65 @@ unless(@data_arr == @metaids_arr) {
     }
     health_failed({data => 'data incomplete, these E2E pairs recorded as metadata but are not returning any data:' . join(', ', keys %$metaids)});
 }
-print encode_json {service => 'OK'};
+print encode_json {service => 'OK'} unless $MAIL;
 exit 0;
 
 sub health_failed {
     my $health = shift;
     $health->{service} = 'NOT OK';
-    print encode_json $health;
+    $Data::Dumper::Terse = 1;
+    print 'MAIL = ' . ref $MAIL;
+    if($MAIL && ref $MAIL eq 'Mail::Sender') {  
+          $MAIL->MailFile({to => $email,
+	                   subject => "PingER check FAILED on ". hostname(), 
+		           msg => "Health Monitor found a problem with PingER MA at $url - " . Dumper($health) . 
+			          "\n logfile is attached ",
+		           file => $logfile,
+			   on_errors => 'die'
+			 });
+    } else {
+        print  encode_json $health; 
+    }
     exit 1;
 }
  
 __END__
+
+
+=head1 SEE ALSO
+
+L<use Getopt::Long>
+
+To join the 'perfSONAR Users' mailing list, please visit:
+
+  https://mail.internet2.edu/wws/info/perfsonar-user
+
+The perfSONAR-PS subversion repository is located at:
+
+  http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk
+
+Questions and comments can be directed to the author, or the mailing list.
+Bugs, feature requests, and improvements can be directed here:
+
+  http://code.google.com/p/perfsonar-ps/issues/list
+
+=head1 VERSION
+
+$Id: $
+
+=head1 AUTHOR
+
+Maxim Grigoriev, maxim_at_fnal_dot_gov
+
+=head1 LICENSE
+
+You should have received a copy of the Fermitools  license
+along with this software.   
+
+=head1 COPYRIGHT
+
+Copyright (c)  2009, Fermitools
+
+All rights reserved.
+
+=cut
