f56e4b36840dbe8e577b3796e0c5c26619a52aad
alake <alake@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2010-07-27 18:04:42 +0000

    Added options to check_throughput plug-in to check for multiple tests by excluding the source and/or destination. Also added an option to check test in both directions
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@4290 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- client/nagios/check_throughput.pl
+++ client/nagios/check_throughput.pl
@@ -15,9 +15,11 @@ use XML::LibXML;
 
 use constant BW_SCALE => 10e8;
 use constant BW_LABEL => 'Gbps';
+use constant HAS_METADATA => 1;
+use constant HAS_DATA => 2;
 
 my $np = Nagios::Plugin->new( shortname => 'PS_CHECK_THROUGHPUT',
-                              usage => "Usage: %s -u|--url <service-url> -s|--source <source-addr> -d|--destination <dest-addr> -r <number-seconds-in-past> -w|--warning <threshold> -c|--critical <threshold>" );
+                              usage => "Usage: %s -u|--url <service-url> -s|--source <source-addr> -d|--destination <dest-addr> -b|--bidirectional -r <number-seconds-in-past> -w|--warning <threshold> -c|--critical <threshold>" );
 
 #get arguments
 $np->add_arg(spec => "u|url=s",
@@ -25,10 +27,13 @@ $np->add_arg(spec => "u|url=s",
              required => 1 );
 $np->add_arg(spec => "s|source=s",
              help => "Source of the test to check",
-             required => 1 );
+             required => 0 );
 $np->add_arg(spec => "d|destination=s",
              help => "Destination of the test to check",
-             required => 1 );
+             required => 0 );
+$np->add_arg(spec => "b|bidirectional",
+             help => "Indicates that test should be checked in each direction.",
+             required => 0 );
 $np->add_arg(spec => "r|range=i",
              help => "Time range (in seconds) in the past to look at data. i.e. 60 means look at last 60 seconds of data.",
              required => 1 );
@@ -43,50 +48,31 @@ $np->getopts;
 #create client
 my $ma_url = $np->opts->{'u'};
 my $ma = new perfSONAR_PS::Client::MA( { instance => $ma_url } );
-
-# Define subject
-my $subject = "<iperf:subject xmlns:iperf=\"http://ggf.org/ns/nmwg/tools/iperf/2.0\" id=\"subject\">\n";
-$subject .=   "    <nmwgt:endPointPair xmlns:nmwgt=\"http://ggf.org/ns/nmwg/topology/2.0/\">";
-$subject .=   "        <nmwgt:src type=\"" . &get_endpoint_type( $np->opts->{'s'} ) . "\" value=\"" . $np->opts->{'s'} . "\"/>";
-$subject .=   "        <nmwgt:dst type=\"" . &get_endpoint_type( $np->opts->{'d'} ) . "\" value=\"" . $np->opts->{'d'} . "\"/>";
-$subject .=   "    </nmwgt:endPointPair>";
-$subject .=   "</iperf:subject>\n";
-
-# Set eventType
-my @eventTypes = ("http://ggf.org/ns/nmwg/tools/iperf/2.0");
-
-my $endTime = time;
-my $startTime = $endTime - $np->opts->{'r'};
-my $result = $ma->setupDataRequest(
-        {
-            start      => $startTime,
-            end        => $endTime,
-            subject    => $subject,
-            eventTypes => \@eventTypes
-        }
-    ) or $np->nagios_die( "Error contacting MA $ma_url" );
-
-my $parser = XML::LibXML->new();
-my $doc;
-eval{
-    $doc = $parser->parse_string(@{$result->{data}});
-};
-if($@){
-    $np->nagios_die( "Error parsing MA response" );
-}
-my $avg_throughput = find($doc->getDocumentElement, "./*[local-name()='datum']/\@throughput", 0);
-if( !defined $avg_throughput){
-    $np->nagios_die( "Error extracting throughput from MA response" );
-}
-if( @{$avg_throughput} == 0 ){
-    $np->nagios_die( "No throughput data returned" );
-}
-
 my $stats = Statistics::Descriptive::Sparse->new();
-foreach my $min_i (@{$avg_throughput}) {
-    $stats->add_data( $min_i->getValue() );
+
+#call client
+&send_data_request($ma, $np->opts->{'s'}, $np->opts->{'d'}, $np->opts->{'r'}, $np->opts->{'b'}, $stats);
+my $srcToDstResults = $stats->count();
+if($stats->count() == 0 ){
+    my $errMsg = "No throughput data returned for direction where";
+    $errMsg .= " src=" . $np->opts->{'s'} if($np->opts->{'s'});
+    $errMsg .= " dst=" . $np->opts->{'d'} if($np->opts->{'d'});
+    $np->nagios_die($errMsg);
 }
 
+#Grab the reverse direction if both source and destination provided
+if($np->opts->{'b'} && $np->opts->{'s'} && $np->opts->{'d'}){
+    &send_data_request($ma, $np->opts->{'d'}, $np->opts->{'s'}, $np->opts->{'r'}, $np->opts->{'b'}, $stats);
+    #If bidirectional, verify we got data from this direction
+    if($srcToDstResults == $stats->count()){
+        my $errMsg = "No throughput data returned for direction where";
+        $errMsg .= " src=" . $np->opts->{'d'};
+        $errMsg .= " dst=" . $np->opts->{'s'};
+        $np->nagios_die($errMsg);
+    }
+}
+
+# format nagios output
 $np->add_perfdata(
         label => 'Count',
         value => $stats->count(),
@@ -122,7 +108,138 @@ if($code eq OK || $code eq WARNING || $code eq CRITICAL){
 }
 $np->nagios_exit($code, $msg);
 
-sub get_endpoint_type(){
+
+#### SUBROUTINES
+sub send_data_request() {
+    my ($ma, $src, $dst, $time_int, $bidir, $stats) = @_;
+    
+    # Define subject
+    my $subject = "<iperf:subject xmlns:iperf=\"http://ggf.org/ns/nmwg/tools/iperf/2.0\" id=\"subject\">\n";
+    if($src && $dst){
+        $subject .=   "    <nmwgt:endPointPair xmlns:nmwgt=\"http://ggf.org/ns/nmwg/topology/2.0/\">";
+        $subject .=   "        <nmwgt:src type=\"" . &get_endpoint_type( $src ) . "\" value=\"" . $src . "\"/>";
+        $subject .=   "        <nmwgt:dst type=\"" . &get_endpoint_type( $dst ) . "\" value=\"" . $dst . "\"/>";
+        $subject .=   "    </nmwgt:endPointPair>";
+    }else{
+        $subject .= "      <nmwgt:endPointPair xmlns:nmwgt=\"http://ggf.org/ns/nmwg/topology/2.0/\"/>\n";
+    }
+    $subject .=   "</iperf:subject>\n";
+    
+    # Set eventType
+    my @eventTypes = ("http://ggf.org/ns/nmwg/tools/iperf/2.0");
+    
+    my $endTime = time;
+    my $startTime = $endTime - $time_int;
+    my $result = $ma->setupDataRequest(
+            {
+                start      => $startTime,
+                end        => $endTime,
+                subject    => $subject,
+                eventTypes => \@eventTypes
+            }
+        ) or $np->nagios_die( "Error contacting MA $ma_url" );
+    
+    # Create parser
+    my $parser = XML::LibXML->new();
+    
+    #determine which endpoints we care about
+    my @endpointsToCheck = ();
+    my $target = "";
+    if($src){
+        $target = $src;
+        push @endpointsToCheck, "src";
+        push @endpointsToCheck, "dst" if($bidir);
+    }elsif($dst){
+        $target = $dst;
+        push @endpointsToCheck, "dst";
+        push @endpointsToCheck, "src" if($bidir);
+    }
+    
+    # parse metadata and determine which tests have matching endpoints
+    my %excludedTests = ();    
+    my %mdIdMap = ();
+    my %mdEndpointMap = ();
+    foreach my $md (@{$result->{"metadata"}}) {
+        if($src && $dst){
+            last;
+        }
+        if(!$src && !$dst && !$bidir){
+            last;
+        }
+        
+        my $mdDoc;
+        eval { $mdDoc = $parser->parse_string($md); };  
+        if($@){
+            $np->nagios_die( "Error parsing metadata in MA response" . $@ );
+        }
+        
+        #record test
+        &record_endpoints($mdDoc, \%mdIdMap, \%mdEndpointMap) if($bidir);
+        
+        if(!$src && !$dst){
+            #we recorded the endpoint for bidirectional test, 
+            # but don't need to exclude anything so go to next iteration
+            next;
+        }
+        
+        #This code sets which tests should be ignored because they don't contain the correct endpoints
+        &check_exclude_test(\@endpointsToCheck, $mdDoc, $target, \%excludedTests);
+    }
+    
+    #parse data
+    foreach my $data ( @{$result->{data}} ){
+        my $doc;
+        eval { $doc = $parser->parse_string( $data ); };  
+        if($@){
+            $np->nagios_die( "Error parsing data in MA response" . $@ );
+        }
+        
+        my $mdIdRef = find($doc->getDocumentElement, "./\@metadataIdRef");
+        if(!$mdIdRef){ 
+            next;
+        }
+        
+        #skip tests without matching endpoints
+        if($excludedTests{"$mdIdRef"}){
+            #make sure that irrelevant tests aren't a factor when checking bidirectionality
+            if($bidir && (!$src || !$dst) && $mdIdMap{$mdIdRef}){
+                $mdEndpointMap{$mdIdMap{$mdIdRef}} = HAS_DATA;
+            }
+            next;
+        }
+        
+        #verify that reverse direction metadata exists. if both src and dst given then checked elsewhere
+        if($bidir && (!$src || !$dst) && (!$mdIdMap{$mdIdRef} || !$mdEndpointMap{$mdIdMap{$mdIdRef}})){
+            $np->nagios_die( "Could not find definition for test " . $mdIdMap{$mdIdRef} . ", but found reverse test." );
+        }
+        
+        my $avg_throughput = find($doc->getDocumentElement, "./*[local-name()='datum']/\@throughput", 0);
+        if( !defined $avg_throughput){
+            $np->nagios_die( "Error extracting throughput from MA response" );
+        }
+        
+        foreach my $through_i (@{$avg_throughput}) {
+            $stats->add_data( $through_i->getValue() );
+            $mdEndpointMap{$mdIdMap{$mdIdRef}} = HAS_DATA if($bidir && (!$src || !$dst));
+        }
+    }
+    
+    #if a bidirectional test, verify all the tests have data
+    if($bidir && (!$src || !$dst)){
+        foreach my $has_data_key (keys %mdEndpointMap){
+            my $rev_key = $has_data_key;
+            $rev_key =~ s/(.+)\-\>(.+)/$2->$1/;
+            
+            #NOTE: No error thrown if neither side has data. Presumably a host
+            #   may be down, etc and we don't want all tests to die
+            if($mdEndpointMap{$has_data_key} != HAS_DATA && $mdEndpointMap{$rev_key} == HAS_DATA){
+                $np->nagios_die("Found data for $has_data_key, but could not find reverse test.");
+            }
+        }
+    }
+}
+
+sub get_endpoint_type() {
     my $endpoint = shift @_;
     my $type = "hostname";
     
@@ -133,4 +250,31 @@ sub get_endpoint_type(){
     }
     
     return $type;
+}
+
+sub check_exclude_test() {
+    my ( $types, $doc, $target, $excludedTests) = @_;
+    
+    if(!$target){
+        return;
+    }
+    
+    my %targetMap = ();
+    foreach my $type(@{$types}){
+        my $ep = find($doc->getDocumentElement, "./*[local-name()='subject']/*[local-name()='endPointPair']/*[local-name()='$type']/\@value");        
+        $targetMap{$ep.""} = $type;
+    }
+    if(!$targetMap{$target}){
+        my $mdId = find($doc->getDocumentElement, "./\@id");
+        $excludedTests->{"$mdId"} = 1;
+    }
+}
+
+sub record_endpoints() {
+    my ($doc, $mdIdMap, $mdEndpointMap) = @_;
+    my $src = find($doc->getDocumentElement, "./*[local-name()='subject']/*[local-name()='endPointPair']/*[local-name()='src']/\@value");
+    my $dst = find($doc->getDocumentElement, "./*[local-name()='subject']/*[local-name()='endPointPair']/*[local-name()='dst']/\@value");
+    my $mdId = find($doc->getDocumentElement, "./\@id");
+    $mdIdMap->{$mdId} = $dst.'->'.$src;
+    $mdEndpointMap->{$src.'->'.$dst} = HAS_METADATA;
 }
