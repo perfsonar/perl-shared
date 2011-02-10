8d13749fbb693e77b44bea5078aeeb4606283976
zurawski <zurawski@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2011-02-10 23:04:47 +0000

    Updating plugin to check both hostname and ip in the registration database.
    -jason
    
    
    
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/sowmya-nagios@4597 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- client/nagios/check_ls.pl
+++ client/nagios/check_ls.pl
@@ -30,11 +30,49 @@ Display:
 
 use FindBin qw($RealBin);
 use lib "$RealBin/../../lib/";
-use Nagios::Plugin;
+
+use lib "/home/jason/release/trunk/Shared/lib";
 use perfSONAR_PS::Common qw( find findvalue );
 use perfSONAR_PS::Client::LS;
+use perfSONAR_PS::Transport;
+use Nagios::Plugin;
 use XML::LibXML;
 use LWP::Simple;
+use Data::Validate::IP qw(is_ipv4 is_ipv6);
+use Socket;
+
+sub get_ip_and_host {
+    my ( $endpoint ) = @_;
+
+    my $ip       = q{};
+    my $hostname = q{};
+
+    if ( is_ipv4( $endpoint ) ) {
+        $ip = $endpoint;
+        my $tmp_addr = Socket::inet_aton( $endpoint );
+        if ( defined $tmp_addr and $tmp_addr ) {
+            $hostname = gethostbyaddr( $tmp_addr, Socket::AF_INET );
+        }
+        $hostname = $endpoint unless $hostname;
+    }
+    elsif ( is_ipv6( $endpoint ) ) {
+        $ip = $endpoint;
+
+        #try to lookup v6 record?
+        $hostname = $endpoint;
+    }
+    else {
+
+        #if not ipv4 or ipv6 then assume a hostname
+        $hostname = $endpoint;
+        my $packed_ip = gethostbyname( $endpoint );
+        if ( defined $packed_ip and $packed_ip ) {
+            $ip = inet_ntoa( $packed_ip );
+        }
+        $ip = $endpoint unless $ip;
+    }
+    return { 'ip' => $ip, 'hostname' => $hostname };
+}
 
 my %NAGIOS_API_ECODES = ( 'OK' => 0, 'WARNING' => 1, 'CRITICAL' => 2, 'UNKNOWN' => 3 );
 
@@ -56,11 +94,13 @@ my $msg    = q{};
 my $code   = q{};
 
 my $ls = new perfSONAR_PS::Client::LS( { instance => $ls_url } );
+my ( $host, $port, $endpoint ) = &perfSONAR_PS::Transport::splitURI( $ls_url );
+my $contact = get_ip_and_host( $host );
 
 my $query = "declare namespace nmwg=\"http://ggf.org/ns/nmwg/base/2.0/\";\n";
 $query = "declare namespace perfsonar=\"http://ggf.org/ns/nmwg/tools/org/perfsonar/1.0/\";\n";
 $query = "declare namespace psservice=\"http://ggf.org/ns/nmwg/tools/org/perfsonar/service/1.0/\";\n";
-$query .= "/nmwg:store[\@type=\"LSStore-summary\"]//psservice:accessPoint[text()=\"" . $ls_url . "\"]\n";
+$query .= "/nmwg:store[\@type=\"LSStore-summary\"]//psservice:accessPoint[text()=\"http://" . $contact->{"hostname"} . ":" . $port . $endpoint . "\"]\n";
 
 my $result = $ls->queryRequestLS( { query => $query, format => 1, eventType => "http://ogf.org/ns/nmwg/tools/org/perfsonar/service/lookup/discovery/xquery/2.0" } );
 if ( not defined $result ) {
@@ -69,9 +109,27 @@ if ( not defined $result ) {
 }
 elsif ( $result->{eventType} =~ m/^error/mx ) {
 
-    # warning, got an answer, not what we wanted
-    $code = $NAGIOS_API_ECODES{WARNING};
-    $msg  = "Service returned unexpected response.";
+    # try the other possibility - the hLS registered w/ IP or HostName.
+    $query = "declare namespace nmwg=\"http://ggf.org/ns/nmwg/base/2.0/\";\n";
+    $query = "declare namespace perfsonar=\"http://ggf.org/ns/nmwg/tools/org/perfsonar/1.0/\";\n";
+    $query = "declare namespace psservice=\"http://ggf.org/ns/nmwg/tools/org/perfsonar/service/1.0/\";\n";
+    $query .= "/nmwg:store[\@type=\"LSStore-summary\"]//psservice:accessPoint[text()=\"http://" . $contact->{"ip"} . ":" . $port . $endpoint . "\"]\n";
+
+    my $result2 = $ls->queryRequestLS( { query => $query, format => 1, eventType => "http://ogf.org/ns/nmwg/tools/org/perfsonar/service/lookup/discovery/xquery/2.0" } );
+    if ( not defined $result2 ) {
+        $code = $NAGIOS_API_ECODES{CRITICAL};
+        $msg  = "Service is not responding.";
+    }
+    elsif ( $result2->{eventType} =~ m/^error/mx ) {
+
+        # warning, got an answer, not what we wanted
+        $code = $NAGIOS_API_ECODES{WARNING};
+        $msg  = "Service returned unexpected response.";
+    }
+    else {
+        $code = $NAGIOS_API_ECODES{OK};
+        $msg  = "Service functioning normally.";
+    }
 }
 else {
     $code = $NAGIOS_API_ECODES{OK};
@@ -85,7 +143,7 @@ __END__
 =head1 SEE ALSO
 
 L<Nagios::Plugin>, L<XML::LibXML>, L<LWP::Simple>, L<perfSONAR_PS::Common>,
-L<perfSONAR_PS::Client::LS>
+L<perfSONAR_PS::Client::LS>, L<Data::Validate::IP>, L<Socket>
 
 To join the 'perfSONAR Users' mailing list, please visit:
 
