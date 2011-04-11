223c29e64085dcc4c29c296957f970c545b67c47
alake <alake@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2011-04-11 18:39:06 +0000

    Fix for issue 475 related to discovering proxy settings. Tested this with both the HTTP_PROXY env variable set and without. Worked in both cases as expected.
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@4735 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- lib/perfSONAR_PS/Transport.pm
+++ lib/perfSONAR_PS/Transport.pm
@@ -246,6 +246,7 @@ sub sendReceive {
     my $method_uri   = "http://ggf.org/ns/nmwg/base/2.0/message/";
     my $httpEndpoint = &getHttpURI( $self->{CONTACT_HOST}, $self->{CONTACT_PORT}, $self->{CONTACT_ENDPOINT} );
     my $userAgent    = LWP::UserAgent->new( 'timeout' =>  $timeout  );
+    $userAgent->env_proxy();
     $logger->debug( "Sending information to \"" . $httpEndpoint . "\": $envelope" );
     my $msg = perfSONAR_PS::Utils::NetLogger::format( "org.perfSONAR.Transport.sendReceive.start", { endpoint => $httpEndpoint, }  );
     $self->{NETLOGGER}->debug( $msg );
