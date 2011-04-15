f13ba04f1210fc44a52bf9bfd19ceb0cd587e25b
zurawski <zurawski@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2011-04-15 12:57:03 +0000

    Adding a ruby perfSONAR client contributed by Sander Boele from SARA.
    -jason
    
    
    
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@4748 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

---
+++ client/ruby_client.rb
@@ -0,0 +1,65 @@
+#! /usr/bin/ruby
+ 
+# == Name  
+# ruby_client.rb  
+#  
+# == Synopsis 
+#   Simple Ruby perfSONAR client 
+#
+# == Usage 
+#  ./ruby_client.rb [ -h | --help ]  
+#                   [ -r | --request | < ] request.xml 
+#                   [ -s | --service | < ] http://HOST:PORT/ENDPOINT
+#
+# == Options
+#  -h,--help::              Show help  
+#  -s,--service=SERVICE::   Service name 
+#  -r,--request=REQUEST::   Request file name 
+#
+# == Author
+#   Sander Boele, sanderb@sara.nl
+#   Jason Zurawski, zurawski@internet2.edu
+#
+# == Copyright
+#   Copyright (c) 2011, SARA and Internet2
+#
+#   All rights reserved.
+#
+#   You should have received a copy of the Internet2 Intellectual Property
+#   Framework along with this software.  If not, see
+#   <http://www.internet2.edu/membership/ip.html>
+
+require 'rdoc/usage'  
+require 'optparse'  
+require 'ostruct' 
+require 'net/https'
+require 'uri'
+
+options = OpenStruct.new()  
+opts = OptionParser.new()  
+opts.on("-h","--help", "Display the usage information") {RDoc::usage('usage')}  
+opts.on("-r","--request", "=REQUEST",  "Request file name") {|argument| options.request = argument}  
+opts.on("-s","--service", "=SERVICE",  "Service name") {|argument| options.service = argument} 
+opts.parse! rescue RDoc::usage('usage')
+
+uri = URI.parse(options.service)
+xmlorig = IO.read(options.request)
+
+# Remove XML declaration line (if present)
+xml = xmlorig.gsub /^.*<\?xml.*/i, ""
+puts xml
+
+SOAP_HEAD = %{ <SOAP-ENV:Envelope xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Header/><SOAP-ENV:Body> }
+SOAP_FOOT = %{ </SOAP-ENV:Body></SOAP-ENV:Envelope> }
+
+http = Net::HTTP.new(uri.host, uri.port)
+http.open_timeout = 3 # in seconds
+http.read_timeout = 3 # in seconds
+
+request = Net::HTTP::Get.new(uri.request_uri)
+request.initialize_http_header({"Content-Type" => "text/xml"})
+request.body = SOAP_HEAD + xml + SOAP_FOOT
+
+response = http.request(request)
+
+puts response.body
