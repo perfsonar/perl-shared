54ebe2f71fe1bf434865d641219435ba600763f1
sowmya <sowmya@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2012-12-07 19:44:52 +0000

    committing perfsonar keynames and values for LS
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/alake-newlsreg@5416 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

---
+++ lib/perfSONAR_PS/Client/LS/PSKeywords/PSKeyNames.pm
@@ -0,0 +1,38 @@
+package perfSONAR_PS::Client::LS::PSKeywords::PSKeyNames;
+
+use strict;
+use warnings;
+
+
+use constant {
+    LS_KEY_GROUP_COMMUNITIES => "group-communities"
+};
+
+# psservice keys
+use constant {
+    LS_KEY_PSSERVICE_EVENTTYPES => "psservice-eventtypes",
+    LS_KEY_MA_TYPE => "ma-type",
+    LS_KEY_MA_TESTS => "ma-tests",
+    LS_KEY_TS_DOMAINS => "ts-domains",
+};
+
+#pshost keys
+use constant {
+    LS_KEY_PSHOST_TOOLKITVERSION => "pshost-toolkitversion",
+};
+
+#psinterface keys
+use constant {
+    LS_KEY_PSINTERFACE_TYPE => "psinterface-type",
+    LS_KEY_PSINTERFACE_URNS => "psinterface-urns"   
+};
+
+#pstest keys
+use constant {
+    LS_KEY_PSTEST_NAME => "pstest-name",
+    LS_KEY_PSTEST_SOURCE => "pstest-source",
+    LS_KEY_PSTEST_DESTINATION => "pstest-destination",
+    LS_KEY_PSTEST_EVENTTYPES => "pstest-eventtypes"
+};
+
+#pspserson keys
