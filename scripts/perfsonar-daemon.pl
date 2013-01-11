7dec04d74ba3b1142e5dadc09643f2cde68ddbcc
sowmya <sowmya@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2013-01-11 16:48:23 +0000

    removed irrelevant lines (memcache)
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@5495 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- scripts/perfsonar-daemon.pl
+++ scripts/perfsonar-daemon.pl
@@ -32,7 +32,6 @@ use Module::Load;
 use HTTP::Daemon;
 use English '-no_match_vars';
 use Carp;
-use Cache::Memcached;
 
 use FindBin qw($Bin);
 use lib "$Bin/../lib";
@@ -292,7 +291,6 @@ my %listeners       = ();
 my %port_configs    = ();
 my %service_configs = ();
 
-my $memd = Cache::Memcached->new({ servers => [ '127.0.0.1:11211']});
 
 unless ( exists $conf{"port"} and $conf{"port"} ) {
     $logger->error( "No ports defined" );
