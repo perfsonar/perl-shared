363223a00a8a11d2f2c46ce69ed098bf04991062
aaron <aaron@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2014-02-03 21:09:06 +0000

    Merge the changes from trunk into the branch
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/aaron-dual_homed@5894 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- client/c_api/ls/perfsonar_ls_client.h
+++
@@ -1,9 +0,0 @@
-#ifndef PERFSONAR_LS_CLIENT_H
-#define PERFSONAR_LS_CLIENT_H
-
-#include "perfsonar_service.h"
-
-int perfsonar_ls_register(const char *url, perfSONARService *service, char **metadata, int metadata_count, char **ls_key);
-int perfsonar_ls_keepalive(const char *url, const char *ls_key);
-
-#endif
