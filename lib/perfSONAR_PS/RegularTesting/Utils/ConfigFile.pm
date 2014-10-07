a22c90487eb2824537276a968eeb6cb7871e34a1
Aaron Brown <aaron@internet2.edu>
2014-10-07 13:57:44 -0400

    Typo in the comment parser

--- lib/perfSONAR_PS/RegularTesting/Utils/ConfigFile.pm
+++ lib/perfSONAR_PS/RegularTesting/Utils/ConfigFile.pm
@@ -54,7 +54,7 @@ sub parse_file {
         s/([^\\])#.*/$1/g;
 
         # Strip out comments that are at the start of the line
-        s/^\s+#.*/$1/g;
+        s/^\s*#.*//g;
 
         # Strip leading and trailing whitespace
         s/^\s+//;
