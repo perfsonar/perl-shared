4b939083dd76427d235e6cd923e66a54747426b5
Andy Lake <andy@es.net>
2015-07-17 12:04:03 -0400

    Adding support for option to set log level of powstream commands spawned in regular_testing

--- lib/perfSONAR_PS/RegularTesting/Tests/Powstream.pm
+++ lib/perfSONAR_PS/RegularTesting/Tests/Powstream.pm
@@ -41,6 +41,7 @@ has 'resolution'        => (is => 'rw', isa => 'Int', default => 60);
 has 'packet_length'     => (is => 'rw', isa => 'Int', default => 0);
 has 'inter_packet_time' => (is => 'rw', isa => 'Num', default => 0.1);
 has 'receive_port_range' => (is => 'rw', isa => 'Str');
+has 'log_level'          => (is => 'rw', isa => 'Str');
 
 has '_individual_tests' => (is => 'rw', isa => 'ArrayRef[HashRef]');
 has '_runner'           => (is => 'rw', isa => 'perfSONAR_PS::RegularTesting::Utils::CmdRunner');
@@ -163,6 +164,8 @@ override 'run_test' => sub {
         push @cmd, ( '-s', $test_parameters->packet_length ) if $test_parameters->packet_length;
         push @cmd, ( '-i', $test_parameters->inter_packet_time ) if $test_parameters->inter_packet_time;
         push @cmd, ( '-P', $test_parameters->receive_port_range ) if $test_parameters->receive_port_range;
+        push @cmd, ( '-g', $test_parameters->log_level ) if $test_parameters->log_level;
+        
         if ($test->local_address) {
             push @cmd, ( '-S', $test->local_address );
         }
