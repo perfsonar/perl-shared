b373a652ce74016b165b4f55fa3b2a2acab6b3ec
Michael Johnson <mj82@grnoc.iu.edu>
2015-08-04 20:18:54 +0000

    Add unauthorized output

--- lib/perfSONAR_PS/NPToolkit/WebService/Auth.pm
+++ lib/perfSONAR_PS/NPToolkit/WebService/Auth.pm
@@ -2,7 +2,7 @@ package perfSONAR_PS::NPToolkit::WebService::Auth;
 use base 'Exporter';
 
 use strict;
-our @EXPORT_OK = ( 'is_authenticated'  );
+our @EXPORT_OK = ( 'is_authenticated', 'unauthorized_output' );
 
 sub is_authenticated {
     my $cgi = shift;
@@ -12,6 +12,19 @@ sub is_authenticated {
         $authenticated = 1;
     }
     return $authenticated;
+}
+
+sub unauthorized_output {
+    my $cgi = shift;
+    my $header = "";
+    if ( !is_authenticated($cgi) ) {
+        $header = $cgi->header(
+            -type => "text/plain",
+            -status => "401 Unauthorized",
+        );
+        $header .= "Unauthorized";
+    }
+    return $header;
 }
 
 1;
