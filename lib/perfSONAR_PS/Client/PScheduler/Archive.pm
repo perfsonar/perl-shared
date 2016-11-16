2167c9a0305c484e63d20983af6b213c788edce6
Andy Lake <andrewrlake@gmail.com>
2016-11-16 12:28:21 -0500

    Fixes perfsonar/mesh-config#37. Added local tracking of private archiver values so can detect when they change in local config, delete the old task and create a new task with the proper parameters

--- lib/perfSONAR_PS/Client/PScheduler/Archive.pm
+++ lib/perfSONAR_PS/Client/PScheduler/Archive.pm
@@ -1,6 +1,9 @@
 package perfSONAR_PS::Client::PScheduler::Archive;
 
 use Mouse;
+use Params::Validate qw(:all);
+use JSON qw(to_json);
+use Digest::MD5 qw(md5_base64);
 
 has 'name' => (is => 'rw', isa => 'Str');
 has 'data' => (is => 'rw', isa => 'HashRef', default => sub { {} });
@@ -17,6 +20,26 @@ sub data_param {
     }
     
     return $self->data->{$field};
+}
+
+sub checksum() {
+    #calculates checksum for comparing tasks, ignoring stuff like UUID and lead url
+    my ($self, @args) = @_;
+    my $parameters = validate( @args, { 'include_private' => 0} );
+    
+    #disable canonical since we don't care at the moment
+    my $archive= { 'name' => $self->name(), 'data' => {} };
+    #clear our private fields that won't get displayed by remote tasks
+    foreach my $datum(keys %{$self->data()}){
+        if($datum =~ /^_/ && !$parameters->{'include_private'}){
+            $archive->{'data'}->{$datum} = '';
+        }else{
+            $archive->{'data'}->{$datum} = $self->data()->{$datum};
+        }
+    }
+        
+    #canonical should keep it consistent by sorting keys
+    return md5_base64(to_json($archive, {'canonical' => 1}));
 }
 
 __PACKAGE__->meta->make_immutable;
